60ace8acbaca88f8d30c6f217f85bd898cdefe22
Ramazan YAYLALI <ryaylali97@gmail.com>
2023-07-10 16:29:46 +0300

    Add project files.

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        [HttpGet("Set/{name}")]
+        public void SetName(string name)
+        {
+            _memoryCache.Set("name", name);
+        }
+        [HttpGet]
+        public string GetName()
+        {
+            return _memoryCache.Get<string>("name");
+        }
+        [HttpGet("TryGetValue")]
+        public string GetName1()
+        {//trygetvalue normal çalışma zamnaında eğer boş değer gelirse boş dönme hatası vermesini önler
+            if (_memoryCache.TryGetValue<string>("name",out string name))
+            {
+                return name.Substring(3);
+            }
+            return "";
+        }
+        [HttpGet("SetDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),//Veri eklendikten 30 saniye sonra veri silinir
+                SlidingExpiration= TimeSpan.FromSeconds(5),//Veri sliding periyodu 5 saniye içinde her hangi bir işlem olmaz ise de silinir
+            });
+        }
+        [HttpGet("GetDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
---
+++ Redis.Pub.Sub.Subscrible.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Pub.Sub.Subscrible.Example
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+            ISubscriber subscriber = redis.GetSubscriber();
+            await subscriber.SubscribeAsync("mychannel", (channel, value) =>
+              {
+                  Console.WriteLine(value);
+              });
+            Console.Read();
+        }
+    }
+}
